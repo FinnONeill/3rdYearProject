09e560f60d49d34d08726b6d8b25e3022f64c517
Finnian O'Neill <finnian.oneill27@mail.dcu.ie>
2017-03-02 19:51:55 +0000

    Removed sample code directories.

--- code/3rd Year Project App/Pos_App/app/src/main/java/com/example/finn/pos_app/SplashScreen.java
+++
@@ -1,9 +0,0 @@
-package com.example.finn.pos_app;
-
-/**
- * Created by Finn on 23/02/2017.
- */
-
-public class SplashScreen {
-
-}
--- code/ThirdYearProject/app/src/main/java/com/example/ian/thirdyearproject/Class.java
+++
@@ -1,8 +0,0 @@
-package com.example.ian.thirdyearproject;
-
-/**
- * Created by Ian on 26/02/2017.
- */
-
-public class Class {
-}
